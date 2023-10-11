cf3e2c0b167d8199ce8911f57c223d9fc4c697f6
mohammedmagdyismael <m.magdy.isl@gmail.com>
2023-10-11 22:40:10 +0300

    Add Pharmacy files uploader

--- src/views/PharmaciesFiles.js
+++ src/views/PharmaciesFiles.js
@@ -1,20 +1,15 @@
 import AWS from "aws-sdk";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
+import { status, getFiles } from './helper'
 
 function App() {
-    const [files, setFilesList] = useState([]);
-    const formatDateTime = (dateString, langCode) => {
-      const day = new Date(`${dateString}Z`).toLocaleString(langCode, { day: '2-digit' });
-      const month = new Date(`${dateString}Z`).toLocaleString(langCode, { month: 'long' });
-      const year = new Date(`${dateString}Z`).toLocaleString(langCode, { year: 'numeric' });
-      const time = new Date(`${dateString}Z`)?.toLocaleTimeString('en-EG', {
-        hour: '2-digit',
-        hour12: true,
-        minute: '2-digit',
-      });
-      return `${day} ${month} ${year}, ${time}`;
-    };
+    let fileInput = useRef();
+    const [uploadFileStatus, setUploadFileStatus] = useState('');
+    const [s3, setS3Instant] = useState(undefined);
+    const [pharmaciesFilesList, setPharmaciesFilesList] = useState([]);
+    const [mappedPharmaciesFilesList, setMappedPharmaciesFilesList] = useState([]);
 
+    // Setup AWS Configs
     useEffect(() => {
       AWS.config.update({
         region: process.env.REACT_APP_AWS_REGION,
@@ -23,34 +18,88 @@ function App() {
           secretAccessKey: process.env.REACT_APP_AWS_S3_SECRET_KEY,
         }),
       });
-      const s3 = new AWS.S3();
-      s3.listObjects(
-        { Bucket: process.env.REACT_APP_BUCKET_NAME, Prefix: process.env.REACT_APP_BUCKET_PREFIX_NAME },
-        (err, data) => {
-          if (err) {
-            console.error('Error listing objects:', err);
-          } else {
-            const temp = [];
-            data.Contents.forEach((object) => {
-              temp.push({ file: `${process.env.REACT_APP_CDN_URL}/${object.Key}`, name: object.Key, date: formatDateTime(object.LastModified, 'en-EG')});
-            });
-            temp.sort((objectA, objectB) => new Date(objectB.date) - new Date(objectA.date))
-            setFilesList(temp);
-          }
-        });
-    }, []);
+      setS3Instant(new AWS.S3())
+    }, [])
+    
+    useEffect(() => {
+      if (uploadFileStatus === status.SUCCESS || uploadFileStatus === '') {
+        getFiles(s3, 'pharmacies_mapped/', setMappedPharmaciesFilesList);
+        getFiles(s3, 'pharmacies_files/', setPharmaciesFilesList);
+      }
+    }, [s3, uploadFileStatus]);
+
+    const uploadFile = file => {
+      const params = {
+        Bucket: process.env.REACT_APP_BUCKET_NAME,
+        Key: `pharmacies_files/${file.name}`,
+        Body: file
+      };
+      setUploadFileStatus(status.SUBMITTING);
+      s3.putObject(params, async (err, data) => {
+        if (err) {
+          setUploadFileStatus(status.FAILED);
+          console.error('Error uploading file:', err);
+        } else {
+          await s3.putObjectAcl(
+            {
+              Bucket: process.env.REACT_APP_BUCKET_NAME,
+              Key: `pharmacies_files/${file.name}`,
+              ACL: 'public-read',
+            }
+          ).promise();
+          setUploadFileStatus(status.SUCCESS);
+          console.log('File uploaded successfully:', data);
+        }
+      });
+    }
 
   return (
     <div className="App">
-      <div>
-        <p id="pharmacies-files-title">Pharmacies Mapped Files:</p>
+
+      <div style={{ marginBottom: '30px' }}>
+        <div>
+          <p id="pharmacies-files-title">Upload Pharmacy File:</p>
+        </div>
+        <input
+            style={{ width: '100%' }}
+            type="file"
+            id="stock_csv"
+            accept=".csv, .xlsx"
+            ref={input => {
+              fileInput = input;
+            }}
+            onChange={e => {
+              uploadFile(e.currentTarget.files[0]);
+            }}
+          />
+        <p style={{ fontSize: '15px' }}>{uploadFileStatus}</p>
       </div>
-      <div id="table-header">
-        <p id="cell-header">File</p>
-        <p id="cell-header">Date (by latest)</p>
-      </div>
-      <div>
-        {files?.map(file => (<div id="table-row" key={file.date}><div style={{ width: '50%' }}><a id="file-name" href={file.file} target="_blank">{file.name}</a></div><p id="file-date">{file.date}</p></div>))}
+
+      <div id="tables-container">
+        <div id="files-table">
+          <div>
+            <p id="pharmacies-files-title">Pharmacies:</p>
+          </div>
+          <div id="table-header">
+            <p id="cell-header-file">File</p>
+            <p id="cell-header-date">Date (by latest)</p>
+          </div>
+          <div>
+            {pharmaciesFilesList?.map(file => (<div id="table-row" key={file.date}><div style={{ width: '100%' }}><a id="file-name" href={file.file} target="_blank">{file.name}</a></div><p id="file-date">{file.date}</p></div>))}
+          </div>
+        </div>
+        <div id="files-table">
+          <div>
+            <p id="pharmacies-files-title">Pharmacies Mapped Files:</p>
+          </div>
+          <div id="table-header">
+            <p id="cell-header-file">File</p>
+            <p id="cell-header-date">Date (by latest)</p>
+          </div>
+          <div>
+            {mappedPharmaciesFilesList?.map(file => (<div id="table-row" key={file.date}><div style={{ width: '100%' }}><a id="file-name" href={file.file} target="_blank">{file.name}</a></div><p id="file-date">{file.date}</p></div>))}
+          </div>
+        </div>
       </div>
     </div>
   );
---
+++ src/views/helper.js
@@ -0,0 +1,34 @@
+export const formatDateTime = (dateString, langCode) => {
+  const day = new Date(`${dateString}`).toLocaleString(langCode, { day: '2-digit' });
+  const month = new Date(`${dateString}`).toLocaleString(langCode, { month: 'long' });
+  const year = new Date(`${dateString}`).toLocaleString(langCode, { year: 'numeric' });
+  const time = new Date(`${dateString}`)?.toLocaleTimeString('en-EG', {
+    hour: '2-digit',
+    hour12: true,
+    minute: '2-digit',
+  });
+  return `${day} ${month} ${year}, ${time}`;
+};
+
+export const getFiles = (s3Instant, directoryPrefix, callBack) => {
+  s3Instant?.listObjects(
+    { Bucket: process.env.REACT_APP_BUCKET_NAME, Prefix: directoryPrefix },
+    (err, data) => {
+      if (err) {
+        console.error('Error listing objects:', err);
+      } else {
+        const temp = [];
+        data.Contents.forEach((object) => {
+          temp.push({ file: `${process.env.REACT_APP_CDN_URL}/${object.Key}`, name: String(object.Key).replace(directoryPrefix, ''), date: formatDateTime(object.LastModified, 'en-EG')});
+        });
+        temp.sort((objectA, objectB) => new Date(objectB.date) - new Date(objectA.date))
+        callBack(temp);
+      }
+    });
+}
+
+export const status = {
+  SUBMITTING: 'Uploading File ....',
+  SUCCESS: 'File Uploaded Successfully !',
+  FAILED: 'File Uploading Failed !',
+}
